4c033459bea86ab7e8852a8a7334b36516389eee
Cyber <ntnkvart>
2019-07-17 20:07:21 +0300

    Add arcControl

--- SpeedTest/SpeedTest/ArcControl/Arc.cs
+++ SpeedTest/SpeedTest/ArcControl/Arc.cs
@@ -1,122 +1,167 @@
-﻿using Windows.Foundation;
+﻿using System;
+using Windows.Foundation;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
 
-namespace ArcControl
+namespace SpeedTest
 {
     public class Arc : Canvas
     {
+        #region Dependency properties
+
         public static readonly DependencyProperty RadiusProperty = DependencyProperty.Register("Radius", typeof(double), typeof(Arc), new PropertyMetadata(50.0, OnSizePropertyChanged));
         public static readonly DependencyProperty ThicknessProperty = DependencyProperty.Register("Thickness", typeof(double), typeof(Arc), new PropertyMetadata(2.0, OnSizePropertyChanged));
-        public static readonly DependencyProperty FillProperty = DependencyProperty.Register("Fill", typeof(Color), typeof(Arc), new PropertyMetadata(default(Brush)));
+        public static readonly DependencyProperty FillProperty = DependencyProperty.Register("Fill", typeof(Color), typeof(Arc), new PropertyMetadata(Color.FromArgb(1, 1, 1, 1)));
         public static readonly DependencyProperty PercentValueProperty = DependencyProperty.Register("PercentValue", typeof(double), typeof(Arc), new PropertyMetadata(0.0, OnPercentValuePropertyChanged));
 
-        public Arc()
-        {
-            this.Loaded += OnLoaded;
-        }
+        #endregion
+
+        #region Properties
 
         public double Radius
         {
-            get
-            {
-                return (double)GetValue(RadiusProperty);
-            }
-            set
-            {
-                SetValue(RadiusProperty, value);
-            }
+            get => (double)GetValue(RadiusProperty);
+            set => SetValue(RadiusProperty, value);
         }
 
         public double Thickness
         {
-            get
-            {
-                return (double)GetValue(ThicknessProperty);
-            }
-            set
-            {
-                SetValue(ThicknessProperty, value);
-            }
+            get => (double)GetValue(ThicknessProperty);
+            set => SetValue(ThicknessProperty, value);
         }
 
         public Color Fill
         {
-            get
-            {
-                return (Color)GetValue(FillProperty);
-            }
-            set
-            {
-                SetValue(FillProperty, value);
-            }
+            get => (Color)GetValue(FillProperty);
+            set => SetValue(FillProperty, value);
         }
 
         public double PercentValue
         {
-            get
-            {
-                return (double)GetValue(PercentValueProperty);
-            }
-            set
-            {
-                SetValue(PercentValueProperty, value);
-            }
+            get => (double)GetValue(PercentValueProperty);
+            set => SetValue(PercentValueProperty, value);
         }
 
-        private void OnLoaded(object sender, RoutedEventArgs e)
-        {
-            SetControlSize();
-            Draw();
-        }
+        #endregion
+
+        #region Helpful methods
 
         private static void OnSizePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var control = d as Arc;
+            Arc control = (Arc)d;
             control.SetControlSize();
             control.Draw();
         }
 
         private static void OnPercentValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var control = d as Arc;
+            Arc control = (Arc)d;
             control.SetControlSize();
             control.Draw();
+        }
+
+        private void SetControlSize()
+        {
+            Width = this.Radius * 2 + this.Thickness;
+            Height = this.Radius * 2 + this.Thickness;
         }
 
         private void Draw()
         {
             Children.Clear();
 
-            Path radialStrip = ArcHelper.GetCircleSegment(GetCenterPoint(), Radius, GetAngle());
-            radialStrip.Stroke = new SolidColorBrush(Fill);
-            radialStrip.StrokeThickness = Thickness;
+            Path radialStrip = GetCircleSegment(this.GetCenterPoint(), this.Radius, this.GetAngle());
+            radialStrip.Stroke = GetGradientBrush(this.Fill);           
+            radialStrip.StrokeThickness = this.Thickness;
 
             Children.Add(radialStrip);
         }
 
-        private void SetControlSize()
-        {
-            Width = Radius * 2 + Thickness;
-            Height = Radius * 2 + Thickness;
-        }
-
         private Point GetCenterPoint()
         {
-            return new Point(Radius + (Thickness / 2), Radius + (Thickness / 2));
+            return new Point(this.Radius + (this.Thickness / 2), this.Radius + (this.Thickness / 2));
         }
 
         private double GetAngle()
         {
-            double angle = PercentValue / 100 * 360;
+            double angle = 0;
+
+            if(this.PercentValue <= 100) // under 100
+            {
+                angle = this.PercentValue / 100 * 225;
+            }
+            else if(this.PercentValue > 100) // before 100 to 400
+            {
+                angle = ((this.PercentValue - 100) / 300 * 135) + (225);
+            }
+
             if (angle >= 360)
             {
                 angle = 359.999;
             }
             return angle;
         }
+
+        private LinearGradientBrush GetGradientBrush(Color fillColor)
+        {
+            GradientStopCollection gradientCollection = new GradientStopCollection();
+            gradientCollection.Add(new GradientStop { Color = Colors.LightBlue, Offset = 0.9 });
+            gradientCollection.Add(new GradientStop { Color = fillColor, Offset = 0.3 });
+
+            LinearGradientBrush linearGradientBrush = new LinearGradientBrush
+            {
+                StartPoint = new Point { X = 0.6, Y = 0.8 },
+                EndPoint = new Point { X = 0.4, Y = 1 },
+                GradientStops = gradientCollection,
+                ColorInterpolationMode = ColorInterpolationMode.ScRgbLinearInterpolation
+            };
+
+            return linearGradientBrush;
+        }
+
+        #endregion
+
+        #region ArcHelper methods
+
+        private const double RADIANS = Math.PI / 180;
+
+        private static Path GetCircleSegment(Point centerPoint, double radius, double angle)
+        {
+            Path path = new Path();
+            PathGeometry pathGeometry = new PathGeometry();
+
+            Point circleStart = new Point(centerPoint.X, centerPoint.Y + radius);
+
+            ArcSegment arcSegment = new ArcSegment
+            {
+                IsLargeArc = angle > 180.0,
+                Point = ScaleUnitCirclePoint(centerPoint, angle, radius),
+                Size = new Size(radius, radius),
+                SweepDirection = SweepDirection.Clockwise               
+            };
+
+            PathFigure pathFigure = new PathFigure
+            {
+                StartPoint = circleStart,
+                IsClosed = false
+            };
+
+            pathFigure.Segments.Add(arcSegment);
+            pathGeometry.Figures.Add(pathFigure);
+
+            path.Data = pathGeometry;
+
+            return path;
+        }
+
+        private static Point ScaleUnitCirclePoint(Point origin, double angle, double radius)
+        {
+            return new Point(origin.Y - Math.Sin(RADIANS * angle) * radius, origin.X + Math.Cos(RADIANS * angle) * radius);
+        }
+
+        #endregion
     }
 }
