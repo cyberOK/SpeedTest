6098a95951b6e099ccd05f62eadd03f18a148d71
Cyber <ntnkvart>
2019-08-28 18:48:03 +0300

    Try to rework arc control. Adjust databoard.

--- SpeedTest/SpeedTest/MainPage.xaml.cs
+++ SpeedTest/SpeedTest/MainPage.xaml.cs
@@ -21,6 +21,7 @@ using System.ServiceModel;
 using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 using Windows.UI.ViewManagement;
+using SpeedTest.RingSliceControl;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -35,6 +36,13 @@ namespace SpeedTest
         {
             this.InitializeComponent();
             ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(380, 500));
+
+            for (int i = 0; i < 360; i++)
+            {
+                double startAngle = i - 180;
+                Brush brush = new SolidColorBrush(Colors.DarkCyan.Interpolate(Colors.Black, (double)i / 360));
+                this.ArcGrid.Children.Add(new RingSlice() { StartAngle = startAngle, EndAngle = startAngle + 1, Fill = brush, Stroke = brush, Radius = 210, InnerRadius = 190 });
+            }
         }
     }
 }
---
+++ SpeedTest/SpeedTest/RingSlice/ColorExtension.cs
@@ -0,0 +1,29 @@
+﻿using Windows.UI;
+
+namespace SpeedTest.RingSliceControl
+{
+    public static class ColorExtension
+    {
+        // Interpolates two colors
+        public static Color Interpolate(this Color color1, Color color2, double fraction)
+        {
+            if (fraction > 1)
+            {
+                fraction = 1;
+            }
+
+            if (fraction < 0)
+            {
+                fraction = 0;
+            }
+
+            Color result = new Color();
+            result.A = 255;
+            result.R = (byte)(color1.R * fraction + color2.R * (1 - fraction));
+            result.G = (byte)(color1.G * fraction + color2.G * (1 - fraction));
+            result.B = (byte)(color1.B * fraction + color2.B * (1 - fraction));
+
+            return result;
+        }
+    }
+}
